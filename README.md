dae0c7a7e2089b32db8f13c7d621f20ec11ade31
Adrian <boniekadi@gmail.com>
2023-04-29 19:07:01 +0200

    Adding Code

--- README.md
+++ README.md
@@ -1,70 +1,27 @@
-# Getting Started with Create React App
+<a href="https://github.com/rishavanand" target="_blank">
+<img src=https://img.shields.io/badge/github-%2324292e.svg?&style=for-the-badge&logo=github&logoColor=white alt=github style="margin-bottom: 5px;" />
+</a>
+<a href="https://twitter.com/iamrishavanand" target="_blank">
+<img src=https://img.shields.io/badge/twitter-%2300acee.svg?&style=for-the-badge&logo=twitter&logoColor=white alt=twitter style="margin-bottom: 5px;" />
+</a>
+<a href="https://linkedin.com/in/iamrishavanand" target="_blank">
+<img src=https://img.shields.io/badge/linkedin-%231E77B5.svg?&style=for-the-badge&logo=linkedin&logoColor=white alt=linkedin style="margin-bottom: 5px;" />
+</a>
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+### Glad to see you here!
 
-## Available Scripts
+I made this project for "Frontend Mentor" to test my skills. The task was quite enjoyable.
 
-In the project directory, you can run:
+Demo : https://szejkoo.github.io/result-summary-component
 
-### `npm start`
+<br/>
 
-Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
+## Languages and Tools
 
-The page will reload when you make changes.\
-You may also see any lint errors in the console.
+<div align="center">  
+<a href="https://reactjs.org/" target="_blank"><img style="margin: 10px" src="https://profilinator.rishav.dev/skills-assets/react-original-wordmark.svg" alt="React" height="50" /></a>  
+<a href="https://styled-components.com/" target="_blank"><img style="margin: 10px" src="https://profilinator.rishav.dev/skills-assets/styled-components.png" alt="Styled Components" height="50" /></a>  
+<a href="https://github.com/" target="_blank"><img style="margin: 10px" src="https://profilinator.rishav.dev/skills-assets/git-scm-icon.svg" alt="Git" height="50" /></a>  
+</div>
 
-### `npm test`
-
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
-
-### `npm run build`
-
-Builds the app for production to the `build` folder.\
-It correctly bundles React in production mode and optimizes the build for the best performance.
-
-The build is minified and the filenames include the hashes.\
-Your app is ready to be deployed!
-
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
-
-### `npm run eject`
-
-**Note: this is a one-way operation. Once you `eject`, you can't go back!**
-
-If you aren't satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you're on your own.
-
-You don't have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn't feel obligated to use this feature. However we understand that this tool wouldn't be useful if you couldn't customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/code-splitting](https://facebook.github.io/create-react-app/docs/code-splitting)
-
-### Analyzing the Bundle Size
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size](https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size)
-
-### Making a Progressive Web App
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app](https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app)
-
-### Advanced Configuration
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/advanced-configuration](https://facebook.github.io/create-react-app/docs/advanced-configuration)
-
-### Deployment
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/deployment](https://facebook.github.io/create-react-app/docs/deployment)
-
-### `npm run build` fails to minify
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+<br/>
---
+++ src/components/Main.js
@@ -0,0 +1,248 @@
+import React from "react";
+import styled from "styled-components";
+import IconMemory from "../img/icon-memory.svg";
+import IconReaction from "../img/icon-reaction.svg";
+import IconVerbal from "../img/icon-verbal.svg";
+import IconVisual from "../img/icon-visual.svg";
+
+const Container = styled.div`
+  @media (min-width: 400px) {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+  }
+  @media (min-width: 700px) {
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: row;
+  }
+`;
+
+const Box = styled.div`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  width: 100vw;
+  height: 41vh;
+  border-bottom-left-radius: 50px;
+  border-bottom-right-radius: 50px;
+  background: rgb(53, 42, 237);
+  background: linear-gradient(
+    0deg,
+    rgba(53, 42, 237, 1) 0%,
+    rgba(102, 67, 255, 1) 100%
+  );
+  @media (min-width: 700px) {
+    border-radius: 50px;
+    height: 50vh;
+  }
+  @media (min-width: 400px) {
+    max-width: 400px;
+  }
+`;
+const SecondBox = styled.div`
+  height: 50vh;
+  margin: 0 50px;
+  @media (min-width: 700px) {
+    margin: 0 10px;
+  }
+`;
+
+const Heading = styled.h3`
+  color: #fff;
+  font-size: 32px;
+  margin: 25px 0 0 0;
+  @media (min-width: 700px) {
+    margin: 70px 0 0 0;
+  }
+`;
+
+const SecondHeading = styled.h3`
+  color: rgba(0, 0, 0, 0.7);
+`;
+const Text = styled.p`
+  color: rgba(255, 255, 255, 0.5);
+  font-size: 18px;
+  font-weight: bold;
+`;
+const CircleText = styled.p`
+  color: rgba(255, 255, 255, 0.5);
+  font-size: 18px;
+  margin: 0;
+`;
+const InfoText = styled.p`
+  display: flex;
+  width: 250px;
+  text-align: center;
+  color: rgba(255, 255, 255, 0.7);
+  margin: 10px;
+`;
+const Num = styled.p`
+  font-size: 64px;
+  font-weight: bold;
+  color: #fff;
+  margin: 0;
+`;
+const Circle = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  flex-direction: column;
+  width: 160px;
+  height: 160px;
+  border-radius: 50%;
+  background: rgb(75, 53, 245);
+  background: linear-gradient(
+    0deg,
+    rgba(75, 53, 245, 1) 0%,
+    rgba(73, 37, 215, 1) 100%
+  );
+`;
+const Panel = styled.div`
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  width: 300px;
+  background-color: #ff57571a;
+  border-radius: 10px;
+`;
+const IconText = styled.div`
+  display: flex;
+  align-items: center;
+`;
+const PanelText = styled.h4`
+  color: hsl(0, 100%, 67%);
+  font-weight: bold;
+  margin: 0 10px;
+`;
+const PanelNum = styled.h4`
+  display: flex;
+  color: rgba(0, 0, 0, 0.5);
+`;
+const SecondPanel = styled.div`
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  width: 300px;
+
+  background-color: #ffb01f1a;
+  border-radius: 10px;
+  margin: 10px 0;
+`;
+const SecondPanelText = styled.h4`
+  color: hsl(39, 100%, 56%);
+  font-weight: bold;
+  margin: 0 10px;
+`;
+const ThridPanel = styled.div`
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  width: 300px;
+
+  background-color: #00bd911a;
+  border-radius: 10px;
+  margin: 10px 0;
+`;
+const ThridPanelText = styled.h4`
+  color: hsl(166, 100%, 37%);
+  font-weight: bold;
+  margin: 0 10px;
+`;
+const FourthPanel = styled.div`
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  width: 300px;
+
+  background-color: #1125d41a;
+  border-radius: 10px;
+  margin: 10px 0;
+`;
+const FourthPanelText = styled.h4`
+  color: hsl(234, 85%, 45%);
+  font-weight: bold;
+  margin: 0 10px;
+`;
+
+const Btn = styled.button`
+  width: 300px;
+  height: 50px;
+  background-color: hsl(224, 30%, 27%);
+  font-weight: bold;
+  color: #fff;
+  border: none;
+  border-radius: 25px;
+  transition: background-color 0.5s;
+  cursor: pointer;
+  &:hover {
+    background-color: hsl(252, 100%, 67%);
+  }
+`;
+
+const TextColor = styled.div`
+  color: #333;
+`;
+function Main() {
+  return (
+    <Container>
+      <Box>
+        <Text>Your Result</Text>
+        <Circle>
+          <Num>76</Num>
+          <CircleText>of 100</CircleText>
+        </Circle>
+        <Heading>Great</Heading>
+        <InfoText>
+          You scored higher than 65% of the people who have taken these tests.
+        </InfoText>
+      </Box>
+      <SecondBox>
+        <SecondHeading>Summary</SecondHeading>
+        <Panel>
+          <IconText>
+            <img src={IconReaction} alt="LightningIcon" />
+            <PanelText>Reaction</PanelText>
+          </IconText>
+          <PanelNum>
+            <TextColor>80</TextColor>/ 100
+          </PanelNum>
+        </Panel>
+        <SecondPanel>
+          <IconText>
+            <img src={IconMemory} alt="BrainIcon" />
+            <SecondPanelText>Memory</SecondPanelText>
+          </IconText>
+          <PanelNum>
+            <TextColor>92</TextColor>/ 100
+          </PanelNum>
+        </SecondPanel>
+        <ThridPanel>
+          <IconText>
+            <img src={IconVerbal} alt="MessageIcon" />
+            <ThridPanelText>Verbal</ThridPanelText>
+          </IconText>
+          <PanelNum>
+            <TextColor>61</TextColor> / 100
+          </PanelNum>
+        </ThridPanel>
+        <FourthPanel>
+          <IconText>
+            <img src={IconVisual} alt="EyeIcon" />
+            <FourthPanelText>Visual</FourthPanelText>
+          </IconText>
+          <PanelNum>
+            <TextColor>72</TextColor> / 100
+          </PanelNum>
+        </FourthPanel>
+        <Btn>Continue</Btn>
+      </SecondBox>
+    </Container>
+  );
+}
+
+export default Main;
